94adb5bf2538204dc2187354f384771a3cd30aff
triplee12 <tripleeoliver@gmail.com>
2023-09-17 14:44:47 +0100

    Implement CORS

--- api/v1/main.py
+++ api/v1/main.py
@@ -2,8 +2,27 @@
 """Estate planning software entry file."""
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from api.v1.configurations.database import engine
+from api.v1.models.data.users import Base as UserBase
+from api.v1.models.data.assets import Base as AssetBase
+
+UserBase.metadata.create_all(bind=engine)
+AssetBase.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = [
+    "*",
+]
+# Cors middleware settings
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
