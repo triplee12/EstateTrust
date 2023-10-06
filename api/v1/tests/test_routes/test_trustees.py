c12f39153c643afc3a946a786f7d61f12ad2c2bb
triplee12 <tripleeoliver@gmail.com>
2023-10-06 13:33:10 +0100

    Implement test cases for trustee routes

--- api/v1/tests/test_routes/test_trustees.py
+++ api/v1/tests/test_routes/test_trustees.py
@@ -79,3 +79,134 @@ def test_create_trustee(
     }
     assert trustee.status_code == status_code
     assert trustee.json() == res_data
+
+
+@pytest.mark.order(after="test_trustees.py::test_create_trustee")
+def test_retrieve_trustees(client):
+    """Test retrieve_trustees."""
+    SECRET_KEY = settings.OAUTH2_SECRET_KEY
+    ALGORITHM = settings.ALGORITHM
+
+    res = client.post('/api/v1/auths/account/login', json={
+        'username': 'eBolton',
+        'password': '07067Oliver',
+        'account_type': 'grantor'
+    })
+    login_data = AccessToken(**res.json())
+    decoded_jwt = jwt.decode(
+        login_data.access_token,
+        SECRET_KEY, algorithms=[ALGORITHM]
+    )
+    user_id = decoded_jwt.get("uuid_pk")
+    headers = {
+        'Authorization': 'Bearer {}'.format(login_data.access_token)
+    }
+    trustees = client.get(
+        f"/api/v1/trustees/account/{user_id}/trustees",
+        headers=headers
+    )
+    assert trustees.status_code == 200
+    assert len(trustees.json()) == 3
+
+
+@pytest.mark.order(after="test_trustees.py::test_retrieve_trustees")
+def test_retrieve_trustee(client):
+    """Test retrieve_trustee."""
+    SECRET_KEY = settings.OAUTH2_SECRET_KEY
+    ALGORITHM = settings.ALGORITHM
+
+    res = client.post('/api/v1/auths/account/login', json={
+        'username': 'eBolton',
+        'password': '07067Oliver',
+        'account_type': 'grantor'
+    })
+    login_data = AccessToken(**res.json())
+    decoded_jwt = jwt.decode(
+        login_data.access_token,
+        SECRET_KEY, algorithms=[ALGORITHM]
+    )
+    user_id = decoded_jwt.get("uuid_pk")
+    headers = {
+        'Authorization': 'Bearer {}'.format(login_data.access_token)
+    }
+    trustees = client.get(
+        f"/api/v1/trustees/account/{user_id}/trustees",
+        headers=headers
+    )
+    trustee_id = trustees.json()[0]["uuid_pk"]
+    trustee = client.get(
+        f"/api/v1/trustees/account/{user_id}/trustees/{trustee_id}",
+        headers=headers
+    )
+
+    assert trustee.status_code == 200
+    assert trustee.json()["added_by"] == user_id
+
+
+@pytest.mark.order(after="test_trustees.py::test_retrieve_trustee")
+def test_update_trustee_account(client):
+    """Test update_trustee_account."""
+    SECRET_KEY = settings.OAUTH2_SECRET_KEY
+    ALGORITHM = settings.ALGORITHM
+
+    res = client.post('/api/v1/auths/account/login', json={
+        'username': 'eBolton',
+        'password': '07067Oliver',
+        'account_type': 'grantor'
+    })
+    login_data = AccessToken(**res.json())
+    decoded_jwt = jwt.decode(
+        login_data.access_token,
+        SECRET_KEY, algorithms=[ALGORITHM]
+    )
+    user_id = decoded_jwt.get("uuid_pk")
+    headers = {
+        'Authorization': 'Bearer {}'.format(login_data.access_token)
+    }
+    trustees = client.get(
+        f"/api/v1/trustees/account/{user_id}/trustees",
+        headers=headers
+    )
+    trustee_id = trustees.json()[0]["uuid_pk"]
+    trustee = client.put(
+        f"/api/v1/trustees/account/{user_id}/trustees/{trustee_id}/update",
+        headers=headers, json={
+            "first_name": "John",
+            "last_name": "Bob"
+        }
+    )
+    assert trustee.status_code == 200
+    assert trustee.json()["first_name"] == "John"
+    assert trustee.json()["last_name"] == "Bob"
+
+
+@pytest.mark.order(after="test_trustees.py::test_update_trustee_account")
+def test_delete_trustee(client):
+    """Test delete trustee account."""
+    SECRET_KEY = settings.OAUTH2_SECRET_KEY
+    ALGORITHM = settings.ALGORITHM
+
+    res = client.post('/api/v1/auths/account/login', json={
+        'username': 'eBolton',
+        'password': '07067Oliver',
+        'account_type': 'grantor'
+    })
+    login_data = AccessToken(**res.json())
+    decoded_jwt = jwt.decode(
+        login_data.access_token,
+        SECRET_KEY, algorithms=[ALGORITHM]
+    )
+    user_id = decoded_jwt.get("uuid_pk")
+    headers = {
+        'Authorization': 'Bearer {}'.format(login_data.access_token)
+    }
+    trustees = client.get(
+        f"/api/v1/trustees/account/{user_id}/trustees",
+        headers=headers
+    )
+    trustee_id = trustees.json()[0]["uuid_pk"]
+    trustee = client.delete(
+        f"/api/v1/trustees/account/{user_id}/trustees/{trustee_id}/delete",
+        headers=headers
+    )
+    assert trustee.status_code == 204
